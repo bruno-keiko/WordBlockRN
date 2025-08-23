17dee30cad3aee25df44236bbffae801246b983f
Begench <gurbanmammedovbegench07@gmail.com>
2025-08-23 18:48:40 +0500

    feat(WordList): ScrollView to flatlist

--- src/widgets/word-list/ui/WordList.tsx
+++ src/widgets/word-list/ui/WordList.tsx
@@ -1,16 +1,62 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList, View, StyleSheet } from 'react-native';
 import { WordCard } from '../../../shared/ui';
 import { Word } from '@/entity/word/interface';
+import { theme } from '@/shared/constants/theme';
+
+const ListFooterComponent = () => {
+  return <View style={styles.footer} />;
+};
+
+const ListEmptyComponent = () => {
+  return <View style={styles.empty} />;
+};
+
+const Separator = () => {
+  return <View style={styles.separator} />;
+};
 
 const WordList = ({ words }: { words: Word[] }) => {
+  const renderWordItem = ({ item }: { item: Word }) => (
+    <WordCard word={item.word} onPress={() => {}} />
+  );
+
+  const keyExtractor = (item: Word) => item.id.toString();
+
+  const handleEndReached = () => {
+    console.log('End reached');
+  };
+
   return (
-    <ScrollView>
-      {words.map((word, index) => (
-        <WordCard key={index} word={word.word} onPress={() => {}} />
-      ))}
-    </ScrollView>
+    <FlatList
+      data={words}
+      contentContainerStyle={styles.container}
+      renderItem={renderWordItem}
+      keyExtractor={keyExtractor}
+      onEndReached={handleEndReached}
+      onEndReachedThreshold={0.5}
+      ListFooterComponent={ListFooterComponent}
+      ListEmptyComponent={ListEmptyComponent}
+      showsVerticalScrollIndicator={false}
+      ItemSeparatorComponent={Separator}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 16,
+  },
+  footer: {
+    height: 50,
+  },
+  empty: {
+    height: 50,
+  },
+  separator: {
+    height: 2,
+    backgroundColor: theme.colors.backgroundSecondary,
+  },
+});
 
 export default WordList;
